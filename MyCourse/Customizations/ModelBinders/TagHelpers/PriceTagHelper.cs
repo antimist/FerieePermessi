cc010c8f12b74c3dea3735a00d52f30281dfc5f2
Stefano Antimi <stefano@antimi.it>
2022-01-30 22:52:18 +0100

    Aggiunti i TagHelper

---
+++ MyCourse/Customizations/ModelBinders/TagHelpers/PriceTagHelper.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyCourse.Models.ViewModels;
+
+
+namespace MyCourse.Customizations.ModelBinders.TagHelpers
+{
+    public class PriceTagHelper : TagHelper
+    {
+        public Money CurrentPrice {get; set;}
+        public Money FullPrice {get; set;}
+        public override void Process (TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.Content.AppendHtml($"{CurrentPrice}");
+
+            if(!CurrentPrice.Equals(FullPrice))
+            {
+                output.Content.AppendHtml($"<br><s>{FullPrice}</s>");
+            }
+        }
+        
+    }
+}
